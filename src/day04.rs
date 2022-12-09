9bf25ddd9ef2ccb321d3e4097f086c31889841e2
Benjamin M. Singleton <bsinglet@vt.edu>
2022-12-09 16:17:13 -0500

    Finished Part 2 of Day 4. I was actually solving a much harder problem than the prompt wanted.

--- src/day04.rs
+++ src/day04.rs
@@ -25,7 +25,7 @@ fn process_lines(lines: &Vec<String>) -> i32 {
     let mut fully_contained: i32 = 0;
     for each_line in lines {
         let commas: String = each_line.replace("-", ",");
-        let mut my_strings: Vec<&str> = commas.split(",").collect();
+        let my_strings: Vec<&str> = commas.split(",").collect();
         let mut numbers: Vec<i32> = Vec::new();
         for each in my_strings {
             numbers.push(i32::from_str(each).unwrap());
@@ -48,15 +48,18 @@ fn process_lines(lines: &Vec<String>) -> i32 {
 }
 
 fn ranges_overlap(a: i32, b: i32, c: i32, d: i32) -> i32 {
-    let mut contained = 0;
+    /*
+    Determine if the range (a, b) overlaps with (c, d) at all.
+    */
+    let mut overlap = 0;
     if (a <= c && c <= b) ||
         (a <= d && d <= b) ||
         (c <= a && a <= d) ||
         (c <= b && b <= d) {
             println!("({}, {}) and ({}, {}) overlap", a, b, c, d);
-            contained += 1;
+            overlap += 1;
         }
-    contained
+    overlap
 }
 
 fn process_lines2(lines: &Vec<String>) -> i32 {
@@ -65,18 +68,21 @@ fn process_lines2(lines: &Vec<String>) -> i32 {
      
      See Part 2 of https://adventofcode.com/2022/day/4
      */
-    let mut contained: i32 = 0;
+    let mut overlaps: i32 = 0;
     let mut pairs: Vec<Vec<i32>> = Vec::new();
     for each_line in lines {
         let commas: String = each_line.replace("-", ",");
-        let mut my_strings: Vec<&str> = commas.split(",").collect();
+        let my_strings: Vec<&str> = commas.split(",").collect();
         let mut numbers: Vec<i32> = Vec::new();
         for each in my_strings {
             numbers.push(i32::from_str(each).unwrap());
         }
-        pairs.push(numbers);
+        overlaps += ranges_overlap(numbers[0], numbers[1], numbers[2], numbers[3]);
     }
 
+    /*
+    // Defunct version. This part of the function solved a much harder 
+    // problem, checking each pair if it overlaps with any other pairs.
     for (index, each_pair) in pairs.iter().enumerate() {
         let (range_1_start, range_1_end, range_2_start, range_2_end) 
             = (pairs[index][0], pairs[index][1], pairs[index][2], pairs[index][3]);
@@ -88,28 +94,15 @@ fn process_lines2(lines: &Vec<String>) -> i32 {
                 | ranges_overlap(range_2_start, range_2_end, a, b) | ranges_overlap(range_2_start, range_2_end, c, d));
             println!("After: {}", contained);
         }
-        /*
-        if numbers[1] - numbers[0] > numbers[3] - numbers[2] {
-            // the first range is bigger, check if it contains the second range
-            if numbers[0] <= numbers[2] && numbers[1] >= numbers[3] {
-                println!("({},{}) fully contains ({},{})", numbers[0], numbers[1], numbers[2], numbers[3]);
-                fully_contained += 1;
-            }
-        }else {
-            // the second range is bigger.
-            if numbers[2] <= numbers[0] && numbers[3] >= numbers[1] {
-                println!("({},{}) fully contains ({},{})", numbers[2], numbers[3], numbers[0], numbers[1]);
-                fully_contained += 1;
-            }
-        }*/
-    }
-    contained
+    }*/
+
+    overlaps
 }
 
 pub fn main() {
     let result = read_lines("day04_input.txt");
     println!("Day 4:");
     println!("Part 1 - The number of fully contained ranges is: {}", process_lines(&result));
-    //println!("Part 2 - The number of pairs that overlap at all is: {}", process_lines2(&result));
+    println!("Part 2 - The number of pairs that overlap at all is: {}", process_lines2(&result));
     println!("");
 }
