583298aa79ddcd6cb73e47d24bdf13e566db5645
Benjamin M. Singleton <bsinglet@vt.edu>
2022-12-14 01:56:26 -0500

    Parts 1 and 2 of Day 6. Very easy.

--- src/day06.rs
+++ src/day06.rs
@@ -1,5 +1,5 @@
 use std::fs;
-use std::str::FromStr;
+use std::collections::HashSet;
 
 fn read_lines(filename: &str) -> Vec<String> {
     /*
@@ -19,10 +19,38 @@ fn process_lines(lines: &Vec<String>) -> i32 {
     /*
      Calculate how many characters are read until detecting the
      start-of-packet marker.
-     
+
      See Part 1 of https://adventofcode.com/2022/day/6
      */
-    0
+    let input = lines[0].clone();
+    let mut result: i32 = 4;
+    for index in 4..input.len() {
+        let rolling_window: HashSet<char> = input[index-4..index].chars().collect();
+        if rolling_window.len() == 4 {
+            result = i32::try_from(index as u32).unwrap();
+            break;
+        }
+    }
+    result
+}
+
+fn process_lines2(lines: &Vec<String>) -> i32 {
+    /*
+     Calculate how many characters are read until detecting the
+     start-of-message marker.
+
+     See Part 2 of https://adventofcode.com/2022/day/6
+     */
+    let input = lines[0].clone();
+    let mut result: i32 = 14;
+    for index in 14..input.len() {
+        let rolling_window: HashSet<char> = input[index-14..index].chars().collect();
+        if rolling_window.len() == 14 {
+            result = i32::try_from(index as u32).unwrap();
+            break;
+        }
+    }
+    result
 }
 
 #[cfg(test)]
@@ -58,11 +86,54 @@ mod tests {
         let lines = vec!["zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".to_string()];
         assert_eq!(process_lines(&lines), 11);
     }
+
+    #[test]
+    fn test_process_lines_full() {
+        let lines = read_lines("day06_input.txt");
+        assert_eq!(process_lines(&lines), 1892);
+    }
+
+    #[test]
+    fn test_process_lines2_example_01() {
+        let lines = read_lines("day06_input_short.txt");
+        assert_eq!(process_lines2(&lines), 19);
+    }
+
+    #[test]
+    fn test_process_lines2_example_02() {
+        let lines = vec!["bvwbjplbgvbhsrlpgdmjqwftvncz".to_string()];
+        assert_eq!(process_lines2(&lines), 23);
+    }
+
+    #[test]
+    fn test_process_lines2_example_03() {
+        let lines = vec!["nppdvjthqldpwncqszvftbrmjlhg".to_string()];
+        assert_eq!(process_lines2(&lines), 23);
+    }
+
+    #[test]
+    fn test_process_lines2_example_04() {
+        let lines = vec!["nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".to_string()];
+        assert_eq!(process_lines2(&lines), 29);
+    }
+
+    #[test]
+    fn test_process_lines2_example_05() {
+        let lines = vec!["zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".to_string()];
+        assert_eq!(process_lines2(&lines), 26);
+    }
+
+    #[test]
+    fn test_process_lines2_full() {
+        let lines = read_lines("day06_input.txt");
+        assert_eq!(process_lines2(&lines), 2313);
+    }
 }
 
 pub fn main() {
-    let result = read_lines("day06_input_short.txt");
+    let result = read_lines("day06_input.txt");
     println!("Day 6:");
     println!("Part 1 - The number of characters processed before the start-of-packet marker is: {}", process_lines(&result));
+    println!("Part 2 - The number of characters processed before the start-of-message marker is: {}", process_lines2(&result));
     println!("");
 }
