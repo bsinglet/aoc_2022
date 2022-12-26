f0ba7edeee7018d43b3760d3bb2438fa7fe74aa7
Benjamin M. Singleton <bsinglet@vt.edu>
2022-12-25 22:51:22 -0600

    Finished Day 9, Part 1.

--- src/day09.rs
+++ src/day09.rs
@@ -16,36 +16,43 @@ fn read_lines(filename: &str) -> Vec<String> {
     lines
 }
 
+fn ascending_order(a: i32, b: i32) -> (i32, i32) {
+    let results: (i32, i32);
+    if a > b {
+        results = (b, a);
+    }else {
+        results = (a, b);
+    }
+    results
+}
+
 fn calculate_distance(head_x_pos: i32, head_y_pos: i32, tail_x_pos: i32, tail_y_pos: i32) -> i32 {
-    let mut distance: i32  = 0;
-    distance = (tail_x_pos - head_x_pos).pow(2) + (tail_y_pos - head_y_pos).pow(2);
-    distance = (distance as f32).sqrt() as i32;
+    let distance: i32;
+    let (a, b) = ascending_order(head_x_pos, tail_x_pos);
+    let (c, d) = ascending_order(head_y_pos, tail_y_pos);
+    if (b - a).abs() <= 1 && (d - c).abs() <= 1 {
+        distance = 0;
+    }else {
+        distance = 2;
+    }
+    //distance = (tail_x_pos - head_x_pos).pow(2) + (tail_y_pos - head_y_pos).pow(2);
+    //distance = (distance as f32).sqrt() as i32;
     distance
 }
 
 fn move_tail(head_x_pos: i32, head_y_pos: i32, tail_x_pos: i32, tail_y_pos: i32) -> (i32, i32, i32, i32) {
-    let mut results = (0, 0, 0, 0);
-    if calculate_distance(head_x_pos, head_y_pos, tail_x_pos, tail_y_pos) > 1 {
+    let mut results = (head_x_pos, head_y_pos, tail_x_pos, tail_y_pos);
+    if calculate_distance(head_x_pos, head_y_pos, tail_x_pos, tail_y_pos) >= 2 {
         // move diagonally
         if head_x_pos > tail_x_pos {
-            results.2 = tail_x_pos + 1;
-        }else {
-            results.2 = tail_x_pos - 1;
+            results.2 += 1;
+        }else if head_x_pos < tail_x_pos {
+            results.2 -= 1;
         }
         if head_y_pos > tail_y_pos {
-            results.3 = tail_y_pos + 1;
-        }else {
-            results.3 = tail_y_pos - 1;
-        }
-    }else {
-        if head_x_pos > tail_x_pos {
-            results.2 = tail_x_pos + 1;
-        }else if head_x_pos < tail_x_pos {
-            results.2 = tail_x_pos - 1;
-        }else if head_y_pos > tail_y_pos {
-            results.3 = tail_y_pos + 1;
+            results.3 += 1;
         }else if head_y_pos < tail_y_pos {
-            results.3 = tail_y_pos - 1;
+            results.3 -= 1;
         }
     }
     results
@@ -65,7 +72,7 @@ fn process_lines(lines: &Vec<String>) -> i32 {
     for each_instruction in lines {
         let direction = each_instruction.split(" ").next().unwrap();
         let distance = i32::from_str(each_instruction.split(" ").skip(1).next().unwrap()).unwrap();
-        println!("{} {}", direction, distance);
+        //println!("{} {}", direction, distance);
         for _move_index in 0..distance {
             match direction {
                 "U" => head_y_pos -= 1,
@@ -75,10 +82,12 @@ fn process_lines(lines: &Vec<String>) -> i32 {
                 _ => println!("Unrecognized direction {}", direction),
             };
 
-            println!("New head position ({}, {})", head_x_pos, head_y_pos);
+            //println!("New head position ({}, {})", head_x_pos, head_y_pos);
 
             // tail needs to catch up with head
             (head_x_pos, head_y_pos, tail_x_pos, tail_y_pos) = move_tail(head_x_pos, head_y_pos, tail_x_pos, tail_y_pos);
+
+            //println!("New tail position ({}, {})", tail_x_pos, tail_y_pos);
 
             // mark tail's current position as visited
             visited_positions.insert((tail_x_pos, tail_y_pos));
@@ -98,21 +107,25 @@ mod tests {
         assert_eq!(process_lines(&lines), 13);
     }
 
-    /*
     #[test]
     fn test_process_lines_full() {
         let lines = read_lines("day09_input.txt");
-        assert_eq!(process_lines(&lines), 1533);
-    }*/
+        assert_eq!(process_lines(&lines), 6470);
+    }
 
     #[test]
     fn test_calculate_distance_01() {
-        assert_eq!(calculate_distance(0, 0, 1, 1), 1);
+        assert_eq!(calculate_distance(0, 0, 1, 1), 0);
     }
 
     #[test]
     fn test_calculate_distance_02() {
         assert_eq!(calculate_distance(-1, -1, 1, 1), 2);
+    }
+
+    #[test]
+    fn test_calculate_distance_03() {
+        assert_eq!(calculate_distance(2, 0, 0, 0), 2);
     }
 
     #[test]
@@ -122,12 +135,12 @@ mod tests {
 
     #[test]
     fn test_move_tail_02() {
-        assert_eq!(move_tail(0, 1, 0, 0), (0, 1, 0, 1));
+        assert_eq!(move_tail(0, 1, 0, 0), (0, 1, 0, 0));
     }
 
     #[test]
     fn test_move_tail_03() {
-        assert_eq!(move_tail(1, 1, 0, 0), (1, 1, 1, 1));
+        assert_eq!(move_tail(1, 1, 0, 0), (1, 1, 0, 0));
     }
 
     #[test]
@@ -148,6 +161,11 @@ mod tests {
     #[test]
     fn test_move_tail_07() {
         assert_eq!(move_tail(0, -5, 0, 5), (0, -5, 0, 4));
+    }
+
+    #[test]
+    fn test_process_lines_01() {
+        assert_eq!(process_lines(&vec!["R 4".to_string(), "U 4".to_string()]), 7);
     }
 }
 
